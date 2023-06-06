1aedd4da7a7fc61dd3093d368aa2ff56ec1d28fb
ericksloninski <erickslk@hotmail.com>
2023-06-06 20:07:42 -0300

    Adcionado o lombok

--- src/main/java/com/client/ws/rasmooplus/model/SubscriptionType.java
+++ src/main/java/com/client/ws/rasmooplus/model/SubscriptionType.java
@@ -1,10 +1,18 @@
 package com.client.ws.rasmooplus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "subscriptions_type")
 @Entity
 public class SubscriptionType implements Serializable {
@@ -24,54 +32,4 @@ public class SubscriptionType implements Serializable {
     @Column(name ="product_key")
     private String productKey;
 
-    public SubscriptionType(Long id, String name, Long acessMonth, BigDecimal price, String productKey) {
-        this.id = id;
-        this.name = name;
-        this.acessMonth = acessMonth;
-        this.price = price;
-        this.productKey = productKey;
-    }
-
-    public SubscriptionType() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAcessMonth() {
-        return acessMonth;
-    }
-
-    public void setAcessMonth(Long acessMonth) {
-        this.acessMonth = acessMonth;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getProductKey() {
-        return productKey;
-    }
-
-    public void setProductKey(String productKey) {
-        this.productKey = productKey;
-    }
 }
