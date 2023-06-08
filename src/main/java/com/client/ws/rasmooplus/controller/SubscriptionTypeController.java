174b59b50b9ede969333b3bebf28d2a059f30a9c
ericksloninski <erickslk@hotmail.com>
2023-06-08 10:25:39 -0300

    Aula 03

---
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -0,0 +1,32 @@
+package com.client.ws.rasmooplus.controller;
+
+import com.client.ws.rasmooplus.model.SubscriptionType;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subscription-type")
+public class SubscriptionTypeController {
+
+    @Autowired
+    private SubscriptionTypeService subscriptionTypeService;
+
+    @GetMapping()
+    public ResponseEntity<List<SubscriptionType>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findById(id));
+
+    }
+}
